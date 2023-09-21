eab1461da2dbdb0311743f3cc2f68f26d49ff9d1
voros-csenge <144840162+voros-csenge@users.noreply.github.com>
2023-09-21 12:40:30 +0200

    Add files via upload
    A simple calculator

---
+++ main.py
@@ -0,0 +1,9 @@
+from Calculator import calculator
+
+Number1 = input("Enter the first number: ")
+Operator = input("Enter an operator: ")
+Number2 = input("Enter the second number: ")
+
+
+'''calculator(4, "/", "2")'''
+calculator(Number1, Operator, Number2)
